4e066f2f378f6049dd626124ca6b0275c3f2c0d9
rijkvanzanten <rijkvanzanten@me.com>
2022-01-30 15:06:54 -0500

    Setup WS service to handle traffic

--- api/src/services/websocket.ts
+++ api/src/services/websocket.ts
@@ -2,16 +2,33 @@ import http from 'http';
 import WebSocket, { WebSocketServer } from 'ws';
 import env from '../env';
 import logger from '../logger';
+import { parse } from 'url';
 
 export class WebSocketService {
 	server: WebSocket.Server;
+	graphqlServer: WebSocket.Server;
 
 	constructor(server: http.Server) {
 		this.server = new WebSocketServer({ server });
+		this.graphqlServer = new WebSocketServer({ noServer: true });
 
 		this.server.on('listening', () => {
 			const port = env.PORT;
 			logger.info(`WebSocket server started at ws://localhost:${port}`);
+		});
+
+		this.server.on('upgrade', (request, socket, head) => {
+			const { pathname } = parse(request.url);
+
+			if (pathname === '/graphql') {
+				this.graphqlServer.handleUpgrade(request, socket, head, (ws) => {
+					ws.emit('connection', ws, request);
+				});
+			} else {
+				this.server.handleUpgrade(request, socket, head, (ws) => {
+					ws.emit('connection', ws, request);
+				});
+			}
 		});
 
 		this.server.on('connection', (ws) => {
